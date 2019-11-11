60345e32a47cf659df50444330e790267d8a0949
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-11-11 15:51:03 +1100

    super simple curses program displaying to screen

--- index.rb
+++ index.rb
@@ -3,10 +3,24 @@ include Curses
 
 # initialises curses
 init_screen
+
+# allows colours
+start_color
+
 begin
   # the number of lines and columns available on the stdscr
   nb_lines = lines
   nb_cols = cols
+
+  x = cols / 2  # We will center our text
+  y = lines / 2
+  setpos(y, x)  # Move the cursor to the center of the screen
+  init_pair(1, Curses::COLOR_RED, Curses::COLOR_GREEN)
+  attrset(color_pair(1) | Curses::A_BOLD)
+  addstr("Hello World")
+  refresh
+  getch
+
 ensure
   close_screen
 end
